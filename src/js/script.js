46564787e9b194911a2987582195336c047d62df
Andrey <noveno1@mail.ru>
2021-01-19 21:31:21 +0300

    validation

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
@@ -15,4 +17,55 @@ const counters = document.querySelectorAll('.skills-ratings__counter'),
 
 counters.forEach( (item, i) => {
     indicators[i].style.width = item.innerHTML;
-})
+});
+
+		function valideForms(form){
+			$(form).validate({
+				rules: {
+					name: {
+						required: true,
+						minlength: 2
+					},
+					email: {
+						required: true,
+						email: true
+					}
+				},
+				messages: {
+					name: {
+						required: "Пожалуйста, введите своё имя",
+						minlength: jQuery.validator.format("Введите {0} символа!")
+					},
+					email: {
+					required: "Пожалуйста, введите свою почту",
+					email: "Неправильно введён адрес почты"
+					}
+				}
+			});
+		};
+
+		valideForms('#contact-form');
+
+
+
+		$('form').submit(function(e) {
+			e.preventDefault();
+					
+			if (!$(this).valid()) {
+				return;
+			}
+
+			$.ajax({
+				type: "POST",
+				url: "mailer/smart.php",
+				data: $(this).serialize()
+			}).done(function() {
+				$(this).find("input").val("");
+				// $('#consultation, #order').fadeOut();
+				// $('.overlay, #thanks').fadeIn('slow');
+				$('form').trigger('reset');
+			});
+			return false;
+		});
+
+});
